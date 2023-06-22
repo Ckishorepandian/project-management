483e9dc6080fbe9c4013ef66902d62a769844602
Kaushik Selvaraju <kaushikselvaraju7@gmail.com>
2023-06-22 15:26:30 +0530

    Integrated TableData

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,84 +18,20 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-
-            <tr>
-              <td>101</td>
-              <td>Project-1</td>
-              <td>Description-1</td>
-              <td>StartDate</td>
-              <td>EndDate</td>
-              <td>
-                <Link
-                  className="btn btn-primary mx-2"
-                >
-                  View
-                </Link>
-                <Link
-                  className="btn btn-outline-primary mx-2"
-                >
-                  Edit
-                </Link>
-                <button
-                  className="btn btn-danger mx-2"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-
-            <tr>
-              <td>102</td>
-              <td>Project-2</td>
-              <td>Description-2</td>
-              <td>StartDate</td>
-              <td>EndDate</td>
-              <td>
-                <Link
-                  className="btn btn-primary mx-2"
-                >
-                  View
-                </Link>
-                <Link
-                  className="btn btn-outline-primary mx-2"
-                >
-                  Edit
-                </Link>
-                <button
-                  className="btn btn-danger mx-2"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-
-            <tr>
-              <td>103</td>
-              <td>Project-3</td>
-              <td>Description-3</td>
-              <td>StartDate</td>
-              <td>EndDate</td>
-              <td>
-                <Link
-                  className="btn btn-primary mx-2"
-                >
-                  View
-                </Link>
-                <Link
-                  className="btn btn-outline-primary mx-2"
-                >
-                  Edit
-                </Link>
-                <button
-                  className="btn btn-danger mx-2"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-
-
-
+            {tableData.map((data) => (
+              <tr key={data.ID}>
+                <td>{data.ID}</td>
+                <td>{data.Name}</td>
+                <td>{data.Description}</td>
+                <td>{data.StartDate}</td>
+                <td>{data.EndDate}</td>
+                <td>
+                  <Link className="btn btn-primary mx-1">View</Link>
+                  <Link className="btn btn-outline-primary mx-2">Edit</Link>
+                  <button className="btn btn-danger mx-2">Delete</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
