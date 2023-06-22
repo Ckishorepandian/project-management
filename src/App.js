b3507fde9194020e0a4cceefb249057f1697589f
Ckishorepandian <kishorepandian421@gmail.com>
2023-06-22 15:09:30 +0530

    uploaded frontend

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,103 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+
+  return (
+    <div className="container">
+      <div className="py-4">
+        <table className="table border shadow">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Name</th>
+              <th scope="col">Description</th>
+              <th scope="col">Start Date</th>
+              <th scope="col">End Date</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+              <tr>
+                <td>101</td>
+                <td>Project-1</td>
+                <td>Description-1</td>
+                <td>StartDate</td>
+                <td>EndDate</td>
+                <td>
+                  <Link
+                    className="btn btn-primary mx-2"
+                  >
+                    View
+                  </Link>
+                  <Link
+                    className="btn btn-outline-primary mx-2"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    className="btn btn-danger mx-2"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+
+               <tr>
+                <td>102</td>
+                <td>Project-2</td>
+                <td>Description-2</td>
+                <td>StartDate</td>
+                <td>EndDate</td>
+                <td>
+                  <Link
+                    className="btn btn-primary mx-2"
+                  >
+                    View
+                  </Link>
+                  <Link
+                    className="btn btn-outline-primary mx-2"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    className="btn btn-danger mx-2"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+
+               <tr>
+                <td>103</td>
+                <td>Project-3</td>
+                <td>Description-3</td>
+                <td>StartDate</td>
+                <td>EndDate</td>
+                <td>
+                  <Link
+                    className="btn btn-primary mx-2"
+                  >
+                    View
+                  </Link>
+                  <Link
+                    className="btn btn-outline-primary mx-2"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    className="btn btn-danger mx-2"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+
+              
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
